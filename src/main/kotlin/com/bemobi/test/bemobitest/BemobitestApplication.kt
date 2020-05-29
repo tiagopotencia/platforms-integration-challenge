eda9a9342ac54d03ad086ce89803db7f385178d5
tiagopotencia <tiagomourabrandao@gmail.com>
2020-05-29 00:01:20 -0300

    Init project

---
+++ src/main/kotlin/com/bemobi/test/bemobitest/BemobitestApplication.kt
@@ -0,0 +1,11 @@
+package com.bemobi.test.bemobitest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BemobitestApplication
+
+fun main(args: Array<String>) {
+	runApplication<BemobitestApplication>(*args)
+}
---
+++ src/test/kotlin/com/bemobi/test/bemobitest/BemobitestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bemobi.test.bemobitest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BemobitestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
